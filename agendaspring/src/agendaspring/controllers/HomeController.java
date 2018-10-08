a9b31a6f21d945fd30ef8e27376783b80acced5a
20151074010123 <20151074010123@MC266454.ifrn.local>
2018-10-08 10:22:37 -0300

    Corrigindo erro que não existia

--- agendaspring/src/agendaspring/controllers/HomeController.java
+++ agendaspring/src/agendaspring/controllers/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 	
-	@RequestMapping("/")
+	@RequestMapping ("/")
 	public String home() {
 		System.out.println("Chamou o meu Controller");
 		return "home.jsp";
